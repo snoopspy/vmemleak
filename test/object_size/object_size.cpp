a1e5cfc2c48d7141814bb89ab06c1708512cc5f9
root <admin@gilgil.net>
2014-12-01 20:38:32 +0900

    Change new and delete operator as global

--- test/object_size/object_size.cpp
+++ test/object_size/object_size.cpp
@@ -2,8 +2,17 @@
 #include <stdio.h>  // printf
 #include <stdlib.h> // malloc
 
-void* operator new(size_t size)   { printf("new(size=%zu)\n", size);   return malloc(size); }
-void* operator new[](size_t size) { printf("new[](size=%zu)\n", size); return malloc(size); }
+void* operator new(size_t size)
+{
+	printf("new(size=%zu)\n", size);
+	return malloc(size);
+}
+
+void* operator new[](size_t size)
+{
+	printf("new[](size=%zu)\n", size);
+	return malloc(size);
+}
 
 class Obj
 {
@@ -36,5 +45,6 @@ int main()
 
 	delete new VObj_dtor;       // 24
 	delete[] new VObj_dtor[10]; // 248
+
 	return 0;
 }
