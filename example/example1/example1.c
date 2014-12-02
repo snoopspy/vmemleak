a1f6ec1a4d2eeea93a73fbc9ac8552ab56fd7c60
gilgil <admin@gilgil.net>
2014-12-02 09:37:37 +0900

    Complete example code

--- example/example1/example1.c
+++ example/example1/example1.c
@@ -1,7 +1,29 @@
 #include "vmld.h"
 
+void malloc_free_test()
+{
+	void* p = malloc(256);
+	free(p);
+}
+
+void calloc_free_test()
+{
+	void* p = calloc(4, 256);
+	free(p);
+}
+
+void realloc_free_test()
+{
+	void* p = malloc(256);
+	p = realloc(p, 256);
+	free(p);
+}
+
 int main()
 {
-	free(malloc(256));
+	malloc_free_test();
+	calloc_free_test();
+	realloc_free_test();
+
 	return 0;
 }
