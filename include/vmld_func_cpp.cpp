1d3a09b7396557de6450efe4c03cc3cb1d2a36cb
root <admin@gilgil.net>
2014-12-05 01:32:49 +0900

    Apply vmld_mgr_... function

--- include/vmld_func_cpp.cpp
+++ include/vmld_func_cpp.cpp
@@ -2,29 +2,36 @@
 #include <stdlib.h> // malloc
 
 #include "vmld_func_cpp.h"
+#include "vmld_mgr.h"
 
 #define debug printf
 
 void* operator new(size_t size, const char* file, const int line) throw(VMLD_NEW_THROW)
 {
 	debug("new(%d, %s, %d)\n", (int)size, file, line);
-	return malloc(size);
+	void* res = malloc(size);
+	res = vmld_mgr_add(res, size, file, line);
+	return res;
 }
 
 void* operator new[](size_t size, const char* file, const int line) throw(VMLD_NEW_THROW)
 {
 	debug("new[](%d, %s, %d)\n", (int)size, file, line);
-	return malloc(size);
+	void* res = malloc(size);
+	res = vmld_mgr_add(res, size, file, line);
+	return res;
 }
 
 void operator delete(void* ptr) throw()
 {
 	debug("delete(%p)\n", ptr);
+	vmld_mgr_del(ptr);
 	free(ptr);
 }
 
 void operator delete[](void* ptr) throw()
 {
 	debug("delete[](%p)\n", ptr);
+	vmld_mgr_del(ptr);
 	free(ptr);
 }
