82799012c6cfac84d06379edeb45c072a72fa56b
gilgil <admin@gilgil.net>
2014-12-04 21:39:03 +0900

    Modifying vmld_mgr

--- include/vmld_mgr.c
+++ include/vmld_mgr.c
@@ -4,6 +4,8 @@
 
 #include "vmld_mgr.h"
 
+#define VMLD_ITEM_CNT 65536
+
 typedef struct
 {
 	void*  ptr;
@@ -12,86 +14,45 @@ typedef struct
 	int    line;
 } vmld_mgr_item_t;
 
-struct vmld_mgr
+typedef struct
 {
-	vmld_mgr_item_t* items;
+	vmld_mgr_item_t items[VMLD_ITEM_CNT];
 	bool del_check;
-	int count;
-	int last_idx;
-};
+	int max_cnt;
+} vmld_mgr_t;
 
-vmld_mgr_item_t* _vmld_mgr_find(void* ptr);
-vmld_mgr_item_t* _vmld_mgr_add(void* ptr, size_t size, char* file, int line);
-vmld_mgr_item_t* _vmld_mgr_del(void* ptr);
+vmld_mgr_item_t* _find(void* ptr);
+vmld_mgr_item_t* _add(void* ptr, size_t size, char* file, int line);
+vmld_mgr_item_t* _del(void* ptr);
 
-static const int VMLD_DEFAULT_CNT = 65536;
+static vmld_mgr_t _vmld_mgr;
 
-bool _vmld_mgr_initialized = false;
-vmld_mgr_t _vmld_mgr = {
-	NULL,
-	false,
-	0,
-	0
-};
-
-vmld_mgr_t* vmld_mgr_instance()
-{
-	if (!_vmld_mgr_initialized)
-	{
-		vmld_mgr_start(&_vmld_mgr, false, VMLD_DEFAULT_CNT);
-	}
-	return &_vmld_mgr;
-}
-
-vmld_mgr_t* vmld_mgr_create()
-{
-	vmld_mgr_t* vmld_mgr = (vmld_mgr_t*)malloc(sizeof(vmld_mgr_t));
-	vmld_mgr_start(vmld_mgr, false, VMLD_DEFAULT_CNT); // initialize memory
-	return vmld_mgr;
-}
-
-void vmld_mgr_destroy(vmld_mgr_t* vmld_mgr)
-{
-	vmld_mgr_stop(vmld_mgr, false); // check last memory leak
-	vmld_mgr_start(vmld_mgr, false, 0); // clear memory
-	free(vmld_mgr);
-}
-
-void vmld_mgr_start(vmld_mgr_t* vmld_mgr, bool del_check, int count)
+void vmld_mgr_start(bool del_check)
 {
 	int i;
+	vmld_mgr_item_t* item;
 
-	if (vmld_mgr->items != NULL)
+	for (i = 0; i < VMLD_ITEM_CNT; i++)
 	{
-		free(vmld_mgr->items);
-		vmld_mgr->items = NULL;
+		item = &_vmld_mgr.items[i];
+		item->ptr = NULL;
+		item->size = 0;
+		item->file = NULL;
+		item->line = 0;
 	}
-	vmld_mgr->del_check = del_check;
-	if (count != 0)
-	{
-		vmld_mgr->items = (vmld_mgr_item_t*)malloc(sizeof(vmld_mgr_item_t) * count);
-		for (i = 0; i < count; i++)
-		{
-			vmld_mgr_item_t* item = &vmld_mgr->items[i];
-			item->ptr = NULL;
-			item->size = 0;
-			item->file = NULL;
-			item->line = 0;
-		}
-	}
-	vmld_mgr->count = count;
-	vmld_mgr->last_idx = 0;
+	_vmld_mgr.del_check = del_check;
+	_vmld_mgr.max_cnt = 0;
 }
 
-void vmld_mgr_stop(vmld_mgr_t* vmld_mgr, bool auto_free)
+void vmld_mgr_stop(bool auto_free)
 {
 	int i;
+	bool write_start;
 
-	vmld_mgr->del_check = false;
-	bool write_start = false;
-	for (i = 0; i < vmld_mgr->last_idx; i++)
+	write_start = false;
+	for (i = 0; i < _vmld_mgr.max_cnt; i++)
 	{
-		vmld_mgr_item_t* item = &vmld_mgr->items[i];
+		vmld_mgr_item_t* item = &_vmld_mgr.items[i];
 		if (item->ptr != NULL)
 		{
 			if (!write_start)
@@ -99,8 +60,8 @@ void vmld_mgr_stop(vmld_mgr_t* vmld_mgr, bool auto_free)
 				write_start = true;
 				printf("******************************************************************************\n");
 			}
-			printf("%p memory leak %p(%d bytes) %s:%d\n",
-				vmld_mgr, item->ptr, (int)item->size, item->file, item->line);
+			printf("memory leak %p(%d bytes) %s:%d\n",
+				item->ptr, (int)item->size, item->file, item->line);
 			if (auto_free)
 			{
 				free(item->ptr);
@@ -115,4 +76,68 @@ void vmld_mgr_stop(vmld_mgr_t* vmld_mgr, bool auto_free)
 	{
 		printf("******************************************************************************\n");
 	}
+	_vmld_mgr.del_check = false;
+}
+
+vmld_mgr_item_t* _vmld_mgr_find(void* ptr)
+{
+	for (int i = 0; i < _vmld_mgr.max_cnt; i++)
+	{
+		vmld_mgr_item_t* item = &_vmld_mgr.items[i];
+		if (item->ptr == ptr)
+		{
+			return info;
+		}
+	}
+	return NULL;
+}
+
+vmld_mgr_item_t* _vmld_mgr_add(void* ptr, size_t size, char* file, int line)
+{
+	for (int i = 0; i < m_cnt; i++)
+	{
+		VMemoryLeakInfo* info = &m_info[i];
+		if (info->p == NULL)
+		{
+			info->p    = p;
+			info->size = size;
+			info->file = file;
+			info->line = line;
+			if (i + 1 > m_cur_max_cnt) m_cur_max_cnt = i + 1;
+			return info;
+		}
+	}
+	return NULL;
+}
+
+VMemoryLeakInfo* VMemoryLeak::del(void* p)
+{
+	for (int i = 0; i < m_cur_max_cnt; i++)
+	{
+		VMemoryLeakInfo* info = &m_info[i];
+		if (info->p == p)
+		{
+			info->p    = NULL;
+			info->size = 0;
+			info->file = NULL;
+			info->line = 0;
+
+			bool m_cur_max_cnt_move = true;
+			VMemoryLeakInfo* temp = info + 1;
+			for (int j = i + 1; j < m_cur_max_cnt; j++)
+			{
+				if (temp->p != NULL)
+				{
+					m_cur_max_cnt_move = false;
+					break;
+				}
+				temp++;
+			}
+
+			if (m_cur_max_cnt_move) m_cur_max_cnt = i + 1;
+
+			return info;
+		}
+	}
+	return NULL;
 }
--- include/vmld_mgr.h
+++ include/vmld_mgr.h
@@ -3,13 +3,8 @@
 
 #include <stdbool.h> // bool
 
-typedef struct vmld_mgr vmld_mgr_t;
-
-vmld_mgr_t* vmld_mgr_instance();
-vmld_mgr_t* vmld_mgr_create();
-void vmld_mgr_destroy(vmld_mgr_t* vmld_mgr);
-void vmld_mgr_start(vmld_mgr_t* vmld_mgr, bool del_check, int count);
-void vmld_mgr_stop(vmld_mgr_t* vmld_mgr, bool auto_free);
+void vmld_mgr_start(bool del_check);
+void vmld_mgr_stop(bool auto_free);
 
 #endif // __VMLD_MGR_H__
 
